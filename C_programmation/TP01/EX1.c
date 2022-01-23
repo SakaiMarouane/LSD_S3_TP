51ee31c78599398d0f9c566cbfba189f077b1493
SakaiMarouane <80362738+SakaiMarouane@users.noreply.github.com>
2022-01-23 23:17:31 +0100

    Create EX1.c

---
+++ C_programmation/TP01/EX1.c
@@ -0,0 +1,18 @@
+1)An integer is stored in 32 bits
+
+2)An integer is stored in 4 bytes
+  
+3)It would be printed :
+  00000000
+  00000010
+  00000100
+  and it gave :0,2,4;
+
+4)if we shift a number by 1 to the left, and we want to convert it to the dicimale againe , we use :
+     Binary: Dn.......D5D4D3D2D1D0
+     Dicimal: Dn*POW(2,n)+D5*POW(2,5)+D4*POW(2,4)+.......D0*POW(2,0)
+     
+5)pow(2,7) is the maximum value that can be stored in a char
+
+6)the first bite is reserved to store the sign 
+ 
