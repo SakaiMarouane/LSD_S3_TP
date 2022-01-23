0cc1b1ced12d4303afa29a9a4fb71db9a76e9710
SakaiMarouane <80362738+SakaiMarouane@users.noreply.github.com>
2022-01-24 00:06:00 +0100

    Update EX3.c

--- C_programmation/TP01/EX3.c
+++ C_programmation/TP01/EX3.c
@@ -1,1 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define N 10
 
+float *lu(float A[N][N], float B[N], int n)
+{
+  return (x);
+}
+
+int main()
+{
+  float   A[N][N], B[N];
+  float   *x;
+  int     n;
+  printf("Enter the size of the matrix: ");
+  scanf("%d", &n);
+  printf("Filling the matrix A\n");
+  for(int i = 0; i < n; i++)
+  {
+    for(int j = 0; j < n; j++)
+    {
+      printf("A[%d][%d] = ", i, j);
+      scanf("%f", &A[i][j]);
+    }
+  }
+  printf("Filling the matrix B\n");
+  for (int i = 0; i < n; i++)
+  {
+    printf("B[%d] = ", i);
+    scanf("%f", &B[i]);
+  }
+  x = lu(A, B, n);
+  printf("\nThe resulting vector: [");
+  for (int i = 0; i < n; i++)
+    printf("%f%c", x[i], ",]"[i == n - 1]);
+}
