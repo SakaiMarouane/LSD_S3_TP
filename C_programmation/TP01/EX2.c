f5df72c877456ec0ff62ea67062e2c723f98ed90
SakaiMarouane <80362738+SakaiMarouane@users.noreply.github.com>
2022-01-24 00:04:37 +0100

    Update EX2.c

--- C_programmation/TP01/EX2.c
+++ C_programmation/TP01/EX2.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 10
 
+float *remontee(float A[N][N], float B[N], int n)
+{
+  return x;
+}
+
+float *gauss(float A[N][N] ,float B[N], int n)
+{
+  x = remontee(A, B, n);
+  return(x);
+}
+
+int main()
+{
+  float   A[N][N], B[N];
+  float   *x;
+  int     n;
+  printf("Enter the size of the matrix: ");
+  scanf("%d", &n);
+  printf("Filling the matrix A\n");
+  for(int i = 0; i < n; i++)
+  {
+    for(int j = 0; j < n; j++)
+    {
+      printf("A[%d][%d] = ", i, j);
+      scanf("%f", &A[i][j]);
+    }
+  }
+  printf("Filling the matrix B\n");
+  for (int i = 0; i < n; i++)
+  {
+    printf("B[%d] = ", i);
+    scanf("%f", &B[i]);
+  }
+  x = gauss(A, B, n);
+  printf("\nThe resulting vector: [");
+  for (int i = 0; i < n; i++)
+    printf("%f%c", x[i], ",]"[i == n - 1]);
+}
