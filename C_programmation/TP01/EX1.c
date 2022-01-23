b785319bc64e232e047ec387948b6bbcea469bae
SakaiMarouane <80362738+SakaiMarouane@users.noreply.github.com>
2022-01-24 00:01:14 +0100

    Update EX1.c

--- C_programmation/TP01/EX1.c
+++ C_programmation/TP01/EX1.c
@@ -1,18 +1,38 @@
-1)An integer is stored in 32 bits
+#include <stdio.h>
+#include <stdlib.h>
+#define N 10
 
-2)An integer is stored in 4 bytes
-  
-3)It would be printed :
-  00000000
-  00000010
-  00000100
-  and it gave :0,2,4;
 
-4)if we shift a number by 1 to the left, and we want to convert it to the dicimale againe , we use :
-     Binary: Dn.......D5D4D3D2D1D0
-     Dicimal: Dn*POW(2,n)+D5*POW(2,5)+D4*POW(2,4)+.......D0*POW(2,0)
-     
-5)pow(2,7) is the maximum value that can be stored in a char
+float *descente(float A[N][N], float B[N], int n)
+{
+  float   *x;
+  x = malloc(sizeof(float) * n);
+  return (x);
+}
 
-6)the first bite is reserved to store the sign 
- 
+
+int main()
+{
+  float   A[N][N], B[N];
+  float   *x;
+  int     n;
+  printf("Enter the size: ");
+  scanf("%d", &n);
+  for(int i = 0; i < n; i++)
+  {
+    for(int j = 0; j < n; j++)
+    {
+      printf("A[%d][%d] = ", i, j);
+      scanf("%f", &A[i][j]);
+    }
+  }
+  for (int i = 0; i < n; i++)
+  {
+    printf("B[%d] = ", i);
+    scanf("%f", &B[i]);
+  }
+  x = descente(A, B, n);
+  printf("\n [");
+  for (int i = 0; i < n; i++)
+    printf("%f%c", x[i], ",]"[i == n - 1]);
+}
